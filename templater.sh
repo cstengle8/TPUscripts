186f3249ae2cb1de37a5b458e7f0266da772230d
Colin Stengle <colinstengle@gmail.com>
2025-01-31 15:39:49 -0800

    Updated scripts

---
+++ templater.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+output="`basename --suffix .mol2 $1`singlechainscript.sh"
+sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.sh > "$output"
+output2="`basename --suffix .mol2 $1`bulkchainscript.sh"
+sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.sh > "$output2"
