6b4394fe2025e87c3697e1cd1e1aa04b3388b8f1
Colin Stengle <colinstengle@gmail.com>
2025-01-31 15:47:35 -0800

    Update templater.sh

--- templater.sh
+++ templater.sh
@@ -2,4 +2,4 @@
 output="`basename --suffix .mol2 $1`singlechainscript.sh"
 sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.sh > "$output"
 output2="`basename --suffix .mol2 $1`bulkscript.sh"
-sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.sh > "$output2"
+sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatebulk.sh > "$output2"
