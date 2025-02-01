3d008b3e99c250e96389c84a1edb526394fcdbd6
Colin Stengle <colinstengle@gmail.com>
2025-01-31 16:02:44 -0800

    Update prepareTPUbatchscripts.sh

--- prepareTPUbatchscripts.sh
+++ prepareTPUbatchscripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 output="`basename --suffix .mol2 $1`singlechainscript.sh"
-sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.sh > "$output"
+sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatesingle.slurm > "$output"
 output2="`basename --suffix .mol2 $1`bulkscript.sh"
-sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatebulk.sh > "$output2"
+sed "s/{input}/$(basename --suffix=.mol2 "$1")/g" templatebulk.slurm > "$output2"
