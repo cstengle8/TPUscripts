cda35bc480da1e2d97b69bc53b32596e9fdf984e
Colin Stengle <colinstengle@gmail.com>
2025-02-06 15:49:50 -0800

    Update template.tcl

--- template.tcl
+++ template.tcl
@@ -1,10 +1,14 @@
-package require topotools
+if {$argc < 1} {
+    puts "Usage: ./script.tcl argument"
+    exit 1
+}
+set arg [lindex $argv 0]
 
-output="TPU4.`basename --suffix .mol2 $1`npt.lammpstrj"
+package require topotools
 
 mol new TPU4.mol2 type mol2 waitfor all
 
-mol addfile TPU4.$1.npt.lammpstrj type lammpstrj waitfor all
+mol addfile "TPU4.${arg}.npt.lammpstrj" type lammpstrj waitfor all
 
 # Get the number of frames
 set nf [molinfo top get numframes]
