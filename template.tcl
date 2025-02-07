82952dbe621867d728f208ca29c3d6a89981b4db
Colin Stengle <colinstengle@gmail.com>
2025-02-06 16:40:57 -0800

    Update template.tcl

--- template.tcl
+++ template.tcl
@@ -2,16 +2,16 @@ if {$argc < 2} {
     puts "Usage: ./script.tcl rtemp mol2"
     exit 1
 }
-set arg [lindex $argv 0]
-set arg2 [lindex $argv 1]
+set rtemp [lindex $argv 0]
+set prefix [lindex $argv 1]
 
 puts "Basename without .mol2: $arg2"
 
 package require topotools
 
-mol new ${arg2}.mol2 type mol2 waitfor all
+mol new ${prefix}.mol2 type mol2 waitfor all
 
-mol addfile "${arg2}.${arg}K.npt.lammpstrj" type lammpstrj waitfor all
+mol addfile "${prefix}.${rtemp}K.npt.lammpstrj" type lammpstrj waitfor all
 
 # Get the number of frames
 set nf [molinfo top get numframes]
@@ -20,7 +20,7 @@ animate goto $nf
 
 # Get atom coordinates from last frame and update
 set sel [atomselect top all frame last]
-$sel writemol2 ${arg2}vmd.mol2
+$sel writemol2 ${prefix}vmd.mol2
 $sel delete
 
 # Exit VMD
